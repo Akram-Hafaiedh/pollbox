0fd2bab9c12fc4c7f5e4f60a92d064b46a50fd92
Akram <hfaiedhakram@gmail.com>
2024-02-14 07:30:03 +0100

    just modified some routing and removed reports dashboard

--- resources/views/admin/reports/dashboard.blade.php
+++ resources/views/admin/reports/dashboard.blade.php
@@ -3,23 +3,28 @@
         <x-dashboard-main-content :page-title="__('Reports Dashboard')">
             <ul class="p-4 pl-5 space-y-4 list-disc bg-gray-100">
 
-                <li class="text-blue-500 hover:underline"><a href="{{ route('admin.reports.quizzes') }}">Quiz Metrics</a>
+                <li class="text-blue-500 hover:underline">
+                    <a href="{{ route('admin.reports.quizzes') }}">New Quiz Reports</a>
                 </li>
-                <li class="text-blue-500 hover:underline"><a href="{{ route('admin.reports.user_metrics') }}">User
-                        Metrics</a></li>
-                <li class="text-blue-500 hover:underline"><a
-                        href="{{ route('admin.reports.participation_metrics') }}">Participation Metrics</a></li>
-                <li class="text-blue-500 hover:underline"><a href="{{ route('admin.reports.question_metrics') }}">Question
-                        Metrics</a></li>
-                <li class="text-blue-500 hover:underline"><a
-                        href="{{ route('admin.reports.quiz_participation_metrics') }}">Quiz Participation Metrics</a>
+                <li class="text-blue-500 hover:underline">
+                    <a href="{{ route('admin.reports.quiz_metrics') }}">Old Quiz Reports</a>
                 </li>
-                <li class="text-blue-500 hover:underline"><a
-                        href="{{ route('admin.reports.user_participation_metrics') }}">User Participation Metrics</a>
+                {{-- <li class="text-blue-500 hover:underline">
+                    <a href="{{ route('admin.reports.participation_metrics') }}">Participation Metrics</a>
                 </li>
-                <li class="text-blue-500 hover:underline"><a
-                        href="{{ route('admin.reports.question_participation_metrics') }}">Question Participation
-                        Metrics</a></li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="{{ route('admin.reports.question_metrics') }}">Question Metrics</a>
+                </li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="{{ route('admin.reports.quiz_participation_metrics') }}">Quiz Participation Metrics</a>
+                </li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="{{ route('admin.reports.user_participation_metrics') }}">User Participation Metrics</a>
+                </li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="{{ route('admin.reports.question_participation_metrics') }}">Question Participation Metrics
+                    </a>
+                </li> --}}
             </ul>
         </x-dashboard-main-content>
     </div>
